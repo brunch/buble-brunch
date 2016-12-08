c2a056dd6b030596c974d078fa6f5f6df3477b5b
Roberto Dip <dip.jesusr@gmail.com>
2016-12-08 17:13:33 -0300

    Improve error reporting and allow custom options

--- index.js
+++ index.js
@@ -3,11 +3,18 @@
 const buble = require('buble');
 
 class BrunchBuble {
-	compile(file) {
-		return Promise.resolve(
-			buble.transform(file)
-		);
-	}
+  constructor({ plugins: { buble } }) {
+    this.config = buble || {};
+  }
+
+  compile({ data }) {
+    try {
+      const { code } = buble.transform(data, this.config);
+      return Promise.resolve(code);
+    } catch (error) {
+      return Promise.reject(error);
+    }
+  }
 }
 
 BrunchBuble.prototype.brunchPlugin = true;
