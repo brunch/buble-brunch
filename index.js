1481b8e2ae672fecdf50e5dd8c4a3d40bd93f1aa
Denys Dovhan <email@denysdovhan.com>
2017-01-25 15:46:34 +0200

    Merge codebase

--- index.js
+++ index.js
@@ -1,24 +1,47 @@
 'use strict';
 
 const buble = require('buble');
+const anymatch = require('anymatch');
+const flatten = require('flatten-brunch-map');
+
+const reIgnore = /\b(?:bower_components|node_modules|vendor)\//;
 
 class BrunchBuble {
   constructor(config) {
-    this.config = config.plugins.buble || {};
+    this.options = Object.assign({}, config.plugins.buble);
+
+    this.options.sourceMap = !!config.sourceMaps
+      && opts.sourceMap !== false
+      && opts.sourceMaps !== false;
+
+    this.pattern = this.options.pattern;
+    this.ignored = anymatch(this.options.ignore || reIgnore);
   }
 
   compile(file) {
-    try {
-      const transform = buble.transform(file.data, this.config);
-      return Promise.resolve(transform.code);
-    } catch (error) {
-      return Promise.reject(error);
-    }
+    return new Promise((resolve, reject) {
+      if (this.ignored(file.path)) {
+        return resolve(file);
+      }
+
+      try {
+        const output = buble.transform(file.data, Object.assign(
+          {}, this.options, {source: file.path}
+        ));
+
+        const sourcemaps = this.options.sourceMap ? output.map : false;
+        const result = flatten(file, output.code);
+
+        return resolve(result);
+      } catch (reason) {
+        return reject(reason);
+      }
+    });
   }
 }
 
 BrunchBuble.prototype.brunchPlugin = true;
 BrunchBuble.prototype.type = 'javascript';
-BrunchBuble.prototype.extension = 'js';
+BrunchBuble.prototype.pattern = /\.jsx?$/;
 
 module.exports = BrunchBuble;
